85e9269bef2a3502b768909efb6a08fd2ccc4c61
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-30 19:34:45 -0500

    still broken

--- common/fabric.directive.js
+++ common/fabric.directive.js
@@ -2,11 +2,10 @@
   "use strict";
   angular
     .module('quiltBuilder')
-    .directive('bananaCar', function() {
+    .directive('quiltFabric', function() {
       return {
-        restrict: 'E',
-        template:"<h1>what the fuck</h1>",
-        // templateUrl: 'common/views/fabric.directive.html',
+        restrict: 'EA',
+        templateUrl: 'common/views/fabric.directive.html',
         scope: {
           fabric: '=',
           action: '&'
@@ -16,21 +15,3 @@
     });
 
 })();
-
-// (function () {
-//   "use strict"; // <flickr-photo>
-//   angular
-//     .module('flickrgram')
-//     .directive('flickrPhoto', function () {
-//       return {
-//         restrict: 'E',
-//         templateUrl: 'common/views/flickrPhoto.directive.html',
-//         scope: {
-//           photo: '=',
-//           action: '&'
-//         },
-//         transclude: true
-//       };
-//     });
-//
-// })();
